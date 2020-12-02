ce35915953c37e8c37abb2bb428698ceb2a381bb
Tom Goldsmith <tom@7theta.com>
2020-12-02 09:18:55 -0500

    Upversion dependencies.

--- project.clj
+++ project.clj
@@ -8,13 +8,13 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/via-auth "0.1.0"
+(defproject com.7theta/via-auth "0.1.1"
   :description "Common authentication strategies for via"
   :url "https://github.com/7theta/via-auth"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.7theta/via "6.1.0"]
+                 [com.7theta/via "6.2.1"]
                  [buddy/buddy-sign "3.2.0"]
                  [buddy/buddy-hashers "1.6.0"]
                  [tick "0.4.24-alpha"]
@@ -26,10 +26,10 @@
                    :env {:malli "true"}
                    :plugins [[lein-environ "0.4.0"]]
                    :dependencies [[binaryage/devtools "1.0.2"]
-                                  [thheller/shadow-cljs "2.11.5"]
+                                  [thheller/shadow-cljs "2.11.8"]
                                   [integrant/repl "0.3.2"]
                                   [org.clojure/clojurescript "1.10.773"]
-                                  [metosin/malli "0.2.0"]]}}
+                                  [metosin/malli "0.2.1"]]}}
   :prep-tasks ["compile"]
   :scm {:name "git"
         :url "https://github.com/7theta/via-auth"})
