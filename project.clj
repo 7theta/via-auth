2954b96f23c48ba7ad67d5ef040748e23fe2effe
Tom Goldsmith <tom@7theta.com>
2021-05-19 16:25:12 -0400

    compiles

--- project.clj
+++ project.clj
@@ -8,13 +8,13 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any others, from this software.
 
-(defproject com.7theta/via-auth "0.2.1"
+(defproject com.7theta/via-auth "0.3.0"
   :description "Common authentication strategies for via"
   :url "https://github.com/7theta/via-auth"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.7theta/via "6.2.3"]
+                 [com.7theta/via "8.0.0"]
                  [buddy/buddy-sign "3.3.0"]
                  [buddy/buddy-hashers "1.7.0"]
                  [integrant "0.8.0"]]
