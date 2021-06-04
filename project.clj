d1c3c9802336b39f8b22a80f8254c1f2101e90d9
Tom Goldsmith <tom@7theta.com>
2021-06-04 10:41:48 -0400

    Upversion via dependency.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.7theta/via "8.1.0"]
+                 [com.7theta/via "8.1.2"]
                  [buddy/buddy-sign "3.3.0"]
                  [buddy/buddy-hashers "1.7.0"]
                  [integrant "0.8.0"]]
